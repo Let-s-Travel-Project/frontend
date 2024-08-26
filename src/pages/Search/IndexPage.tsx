577ebb59c38b0d91a508537039ea6233b4e1425f
sorocode <tudgus010707@naver.com>
2024-08-26 19:03:55 +0900

    💄 add hr

--- src/pages/Search/IndexPage.tsx
+++ src/pages/Search/IndexPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 const IndexPage: React.FunctionComponent = () => {
   return (
     <>
+      <hr className="bg-gray-500 max-sm:h-[1px] h-[1.5px] opacity-40 mx-2 my-4" />
       <p>인덱스페이지</p>
     </>
   );
