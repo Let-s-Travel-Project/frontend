0b62e52a46fc9ad86a1662375c6c86885a4270a6
sorocode <tudgus010707@naver.com>
2024-08-26 16:09:41 +0900

    ➕ setting for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
