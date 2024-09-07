cc787a7e3816d0395e80755fe4e58ee4aebb3bdf
sorocode <tudgus010707@naver.com>
2024-09-07 15:40:08 +0900

    💄 shadcn->native 버튼으로 변경

--- src/components/common/AddButton.tsx
+++ src/components/common/AddButton.tsx
@@ -1,14 +1,10 @@
-import { Button } from "@/lib/utils/ui/button";
 import AddButtonImg from "@/assets/AddButton.png";
 
 const AddButton = () => {
   return (
-    <Button
-      variant="ghost"
-      className="fixed bottom-8 right-0  rounded-full h-30 px-0 mx-0"
-    >
+    <button className="fixed bottom-8 right-0  rounded-full h-30 px-0 mx-0 hover:opacity-80">
       <img src={AddButtonImg} alt="Add Button" className="w-1/2" />
-    </Button>
+    </button>
   );
 };
 
