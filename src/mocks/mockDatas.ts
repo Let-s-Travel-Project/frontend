965387078541d44d1fd99c4091965f5b99b096f3
sorocode <tudgus010707@naver.com>
2024-09-07 15:16:02 +0900

    🍱 도시 이미지 추가

--- src/mocks/mockDatas.ts
+++ src/mocks/mockDatas.ts
@@ -10,63 +10,72 @@ export const mockCityDatas = [
     id: 2,
     cityName: "Osaka",
     cityNameTranslated: "오사카",
-    cityPhotoURL: "osaka.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1704004363915-c4d848da44ea?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "JP",
   },
   {
     id: 3,
     cityName: "Seoul",
     cityNameTranslated: "서울",
-    cityPhotoURL: "seoul.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1485186337913-a42c84d7ffcc?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "KR",
   },
   {
     id: 4,
     cityName: "New York",
     cityNameTranslated: "뉴욕",
-    cityPhotoURL: "newyork.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1516893842880-5d8aada7ac05?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "US",
   },
   {
     id: 5,
     cityName: "Paris",
     cityNameTranslated: "파리",
-    cityPhotoURL: "paris.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1522582324369-2dfc36bd9275?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "FR",
   },
   {
     id: 6,
     cityName: "London",
     cityNameTranslated: "런던",
-    cityPhotoURL: "london.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1526129318478-62ed807ebdf9?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "GB",
   },
   {
     id: 7,
     cityName: "Rome",
     cityNameTranslated: "로마",
-    cityPhotoURL: "rome.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1529154036614-a60975f5c760?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "IT",
   },
   {
     id: 8,
     cityName: "Berlin",
     cityNameTranslated: "베를린",
-    cityPhotoURL: "berlin.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1528728329032-2972f65dfb3f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVybGlufGVufDB8fDB8fHwy",
     countryCode: "DE",
   },
   {
     id: 9,
     cityName: "Sydney",
     cityNameTranslated: "시드니",
-    cityPhotoURL: "sydney.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "AU",
   },
   {
     id: 10,
     cityName: "Moscow",
     cityNameTranslated: "모스크바",
-    cityPhotoURL: "moscow.jpg",
+    cityPhotoURL:
+      "https://images.unsplash.com/photo-1513326738677-b964603b136d?q=80&w=1649&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     countryCode: "RU",
   },
   {
--- src/pages/Search/IndexPage.tsx
+++ src/pages/Search/IndexPage.tsx
@@ -14,6 +14,7 @@ const IndexPage: React.FunctionComponent = () => {
     <>
       <SelectSectorContainer data={sectors} />
       <CityCardsContainer cities={mockCityDatas} />
+      <CityCardsContainer cities={mockCityDatas} />
     </>
   );
 };
